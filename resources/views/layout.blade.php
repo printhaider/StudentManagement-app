998503c0bebe8721e5c26524550479a5170cb9dc
printhaider <haider.bajwa@ontariotechu.net>
2024-07-08 22:35:45 -0400

    Initial commit

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Student Management System</title>
+    <style>
+        /* The side navigation menu */
+        .sidebar {
+            margin: 0;
+            padding: 0;
+            width: 200px;
+            background-color: #f1f1f1;
+            position: fixed;
+            height: 100%;
+            overflow: auto;
+        }
+
+        /* Sidebar links */
+        .sidebar a {
+            display: block;
+            color: black;
+            padding: 16px;
+            text-decoration: none;
+        }
+
+        /* Active/current link */
+        .sidebar a.active {
+            background-color: #04AA6D;
+            color: white;
+        }
+
+        /* Links on mouse-over */
+        .sidebar a:hover:not(.active) {
+            background-color: #555;
+            color: white;
+        }
+
+        /* Page content */
+        .content {
+            margin-left: 200px;
+            padding: 16px;
+            height: 1000px;
+        }
+
+        /* On screens that are less than 700px wide */
+        @media screen and (max-width: 700px) {
+            .sidebar {
+                width: 100%;
+                height: auto;
+                position: relative;
+            }
+            .sidebar a {
+                float: left;
+            }
+            .content {
+                margin-left: 0;
+            }
+        }
+
+        /* On screens that are less than 400px wide */
+        @media screen and (max-width: 400px) {
+            .sidebar a {
+                text-align: center;
+                float: none;
+            }
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <a class="navbar-brand" href="#">
+                        <h1>Student Management Project</h1>
+                    </a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                </nav>
+            </div>
+        </div>
+        <div class="row">
+            <!-- The sidebar -->
+            <div class="col-md-3">
+                <div class="sidebar">
+                    <a class="active" href="#home">Home</a>
+                    <a href="#news">Student</a>
+                    <a href="#contact">Teacher</a>
+                    <a href="#courses">Courses</a>
+                    <a href="#enrollment">Enrollment</a>
+                    <a href="#payment">Payment</a>
+                </div>
+            </div>
+            <!-- Page content -->
+            <div class="col-md-9">
+                <div class="content">
+                    @yield('content')
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
